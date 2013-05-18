e9ea8e525350a7da29e50c1a13be4821a948082b
WuJing <wujing@jike.com>
2013-05-18 22:30:13 +0800

    fix text color

--- src/com/race604/picgallery/ui/ImageGridActivity.java
+++ src/com/race604/picgallery/ui/ImageGridActivity.java
@@ -70,7 +70,6 @@ public class ImageGridActivity extends SlidingFragmentActivity {
 		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
 	}
 	
 	@Override
